983c68075339096d1d2923323bcb325eefc529d8
Julia N <65161271+julexx2000@users.noreply.github.com>
2020-06-03 13:09:23 +0200

    Update projekt 1.py

--- projekt 1.py
+++ projekt 1.py
@@ -14,26 +14,29 @@ glowne_okno.title("POPMASZYNA")
 
 nazwa = Label(glowne_okno, text = "POPMASZYNA, WYBIERZ OPCJĘ:")
 nazwa.grid()
-
 def Muzyczka1():
-    mixer.music.load(" ") #tu jest miejsce na sample
+    mixer.music.load("Bamboo.wav ") #tu jest miejsce na sample
     mixer.music.play()
 
 
 def Muzyczka2():
-    mixer.music.load(" ") #tu jest miejsce na sample
+    mixer.music.load("Claves.wav ") #tu jest miejsce na sample
     mixer.music.play()
 
 def Muzyczka3():
-    mixer.music.load(" ") #tu jest miejsce na sample
+    mixer.music.load("gitara.wav ") #tu jest miejsce na sample
     mixer.music.play()
 
 def Muzyczka4():
-    mixer.music.load(" ") #tu jest miejsce na sample
+    mixer.music.load("elektryk.wav ") #tu jest miejsce na sample
     mixer.music.play()
 
 def Muzyczka5():
-    mixer.music.load(" ") #tu jest miejsce na sample
+    mixer.music.load("syntezator.wav ") #tu jest miejsce na sample
+    mixer.music.play()
+
+def Muzyczka6():
+    mixer.music.load("dzwonki.wav ") #tu jest miejsce na sample
     mixer.music.play()
 
 def losowa_melodia1(fs = 44100, amplituda_sygnalu = 1, dlugosc_dzwieku = 1):
@@ -52,20 +55,23 @@ def losowa_melodia2(fs = 44100, amplituda_sygnalu = 2,dlugosc_dzwieku=1):
         sd.play(sygnal)
         sd.wait()
 
-
-przycisk1 = Button(glowne_okno, text = "pierwszy dźwięk", fg = "gold",  command = Muzyczka1)
+przycisk1 = Button(glowne_okno, text = "BĘBNY", fg = "gold",  command = Muzyczka1)
 przycisk1.grid(row = 1, column = 0)
-przycisk2 = Button(glowne_okno, text = "drugi dźwięk", fg = "indian red", command = Muzyczka2)
+przycisk2 = Button(glowne_okno, text = "COŚ", fg = "indian red", command = Muzyczka2)
 przycisk2.grid(row = 2, column =0 )
-przycisk3 = Button(glowne_okno, text = "trzeci dźwięk", fg = "coral", command = Muzyczka3)
+przycisk3 = Button(glowne_okno, text = "GITARA", fg = "coral", command = Muzyczka3)
 przycisk3.grid(row = 1, column = 1 )
-przycisk4 = Button(glowne_okno, text = "czwarty dźwięk", fg = "salmon", command = Muzyczka4)
+przycisk4 = Button(glowne_okno, text = "GITARA ELEKTRYCZNA", fg = "salmon", command = Muzyczka4)
 przycisk4.grid(row = 2, column = 1)
-przycisk5 = Button(glowne_okno, text = "piąty dźwięk", fg = "red", command = Muzyczka5)
+przycisk5 = Button(glowne_okno, text = "SYNTEZATOR", fg = "red", command = Muzyczka5)
 przycisk5.grid(row = 1, column = 2)
+przycisk5 = Button(glowne_okno, text = "DZWONKI", fg = "red", command = Muzyczka6)
+przycisk5.grid(row = 3, column = 2)
 przycisk6 = Button(glowne_okno, text = "krótka losowa melodia", fg = "hot pink", command = losowa_melodia1)
 przycisk6.grid(row = 2 , column = 2)
 przycisk7 = Button(glowne_okno, text = "długa losowa melodia", fg = "maroon", command = losowa_melodia2)
 przycisk7.grid(row = 1, column = 3)
+
+
 
 glowne_okno.mainloop()
