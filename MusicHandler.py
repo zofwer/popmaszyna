8b102f3e2337d5ad7f6eff3a45990c73f29d8ff5
nerdolo <cloverremdel@gmail.com>
2020-06-04 18:53:33 +0200

    polish instructions in terminal version and checking if given sample number is valid, added checking if sample is .wav file while scanning samples directory

--- MusicHandler.py
+++ MusicHandler.py
@@ -5,21 +5,26 @@ import os
 class MusicHandler:
     def __init__(self, savepath):  # constructor for out object. Defined path to save file as savepath, saved_samples
         # is a list of detected samples, samples is a list of samples to merge, to_save is our buffer that will be
-        # saved to file
+        # saved to file after we add samples to each other
         self.savepath = os.path.normpath(savepath)
         self.saved_samples = []
         self.samples = []
         self.to_save = AudioSegment.empty()
 
     def scanSavedSample(
-            self):  # scanning dictionary 'samples' for samples XD, you might add checking, if founded file is .wav
+            self):  # scanning dictionary 'samples' for samples
         for path in os.listdir('samples'):
-            full_path = os.path.join('samples', path)
-            if os.path.isfile(full_path):
-                self.saved_samples.append(os.path.normpath(full_path))
+            if path[-4:] == '.wav': # check if the file is .wav file
+                full_path = os.path.join('samples', path)
+                if os.path.isfile(full_path):
+                    self.saved_samples.append(os.path.normpath(full_path))
 
     def printSavedSamples(self):  # printing all detected samples (inline for)
-        return [print(f'{iteration}: {path}') for iteration, path in enumerate(self.saved_samples)]
+        return [print(f'{iteration+1}: {path}') for iteration, path in enumerate(self.saved_samples)]
+
+
+    def returnSavedSamples(self):  # return all detected samples as iterable (inline for)
+        return [(f'{iteration+1}: {path}') for iteration, path in enumerate(self.saved_samples)]
 
     def addSampleToMerge(self, num):  # append sample described as number in saved_samples list to samples
         return True if self.samples.append(self.saved_samples[num]) else False
--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import MusicHandler
 
-
 def main():
+    print("Witaj w sklejaku sampli!\nSklei on dla ciebie dostępne sample w dłuższy plik .wav")
     cont = True  # describes if we want to add more samples to merge
-    outputpath = input('Define output filepath (*.wav):\n')
+    outputpath = input('Zdefiniuj ścieżkę wyjściową (*.wav):\n')
     musichandler = MusicHandler.MusicHandler(outputpath)  # creating object to work on
     musichandler.scanSavedSample()  # scanning for samples in folder "samples"
 
     while cont:
+        print('Wpisz numer dostępnego sampla którego chcesz dokleić')
         musichandler.printSavedSamples()
-        num = input('Number of sample:\n')
-        musichandler.addSampleToMerge(int(num))
-        dec = input('Do you want to contunue?\n')
-        if int(dec) != 1:
+        num = input('Numer sampla do dodania:\n')
+        while int(num) < 1 or int(num) > len(musichandler.returnSavedSamples()): # if given number is invalid
+            num = input('Podaj POPRAWNY numer sampla do dodania:\n') # ask for valid number again
+        musichandler.addSampleToMerge(int(num)-1)
+        decision = input('Czy chcesz kontynować sklejanie?\n')
+        if int(decision) != 1:
             cont = False
-            print('Stopped')
+            print('Koniec')
         else:
-            print('Continuing')
+            print('Kontynuujemy')
 
     try:
         musichandler.mergeSamples()  # merging chosen samples into one file
         musichandler.saveMergedSamples()  # exporting merged samples to output file
-        print(f'Saved file to ./{outputpath} :)')
+        print(f'Twoja sklejka została zapisana do ./{outputpath}')
     except Exception as e:
         print(e)
 
